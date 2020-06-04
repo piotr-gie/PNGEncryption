5c42721c7c17bc5a3a47a660eea5ced895b87a65
Piotr Grzelczak <241612@student.pwr.edu.pl>
2020-06-04 17:36:26 +0200

    For my image

--- newPNGEncryption-master/PixEncryptor.cpp
+++ newPNGEncryption-master/PixEncryptor.cpp
@@ -11,39 +11,46 @@ PixelEncryptor::PixelEncryptor(string fileName_) : fileName{fileName_}
 {
 }
 
-int encryptPixel(mpz_class pixelinput)
+mpz_class PixelEncryptor::encryptPixel(mpz_class pixelinput)
 {
   RSAEncryptor encryptor;
             mpz_class encryptedData;
-            int encryptedpixelvalue;
             mpz_t encrypted;
             mpz_init(encrypted);
             mpz_powm(encrypted, pixelinput.get_mpz_t(),encryptor.e.get_mpz_t(), encryptor.n.get_mpz_t());
             mpz_class ciphers{encrypted};
             encryptedData= ciphers;
-            encryptedpixelvalue= encryptedData.get_ui();
-            return encryptedpixelvalue;
+
+            encryptions.push_back(encryptedData);
+
+            return encryptedData;
+
+            // int encryptedpixelvalue;
+            // encryptedpixelvalue= encryptedData.get_ui();
+            // return encryptedpixelvalue;
 }
 
-int decryptPixel(mpz_class pixelinput)
+mpz_class PixelEncryptor::decryptPixel(mpz_class pixelinput)
 {
   RSAEncryptor encryptor;
             mpz_class decryptedData;
-            int decryptedpixelvalue;
             mpz_t decrypted;
             mpz_init(decrypted);
             mpz_powm(decrypted, pixelinput.get_mpz_t(), encryptor.key.get_mpz_t(), encryptor.n.get_mpz_t());
             mpz_class decryptedmessage{decrypted};
             decryptedData=decryptedmessage;
-            decryptedpixelvalue= decryptedData.get_ui();
-            return decryptedpixelvalue;
+            return decryptedData;
+
+            // unsigned int decryptedpixelvalue;
+            // decryptedpixelvalue= decryptedData.get_ui();
+            // return decryptedpixelvalue;
 }
 
 void PixelEncryptor::encryptPixels()
 {
   //PNGEncryptor pngencryptor(fileName);
   Image image;
-  
+
 try {
     // Read a file into image object
     image.read(fileName);
@@ -52,13 +59,14 @@ try {
     //forwardFourierTransformImage(&FFTresult,image);
     //Make magnitude visible by using logarythmic scale
     Pixels view(image);
-    PixelPacket *pixels = view.get(0,0,image.columns(),image.rows());
+    Quantum *pixels = view.get(0,0,image.columns(),image.rows());
     for(size_t i=0; i<image.columns()*image.rows();i++)
   {
-        pixels->red=encryptPixel(pixels->red);
-        pixels->green=encryptPixel(pixels->green);
-        pixels->blue=encryptPixel(pixels->blue);
-        pixels++;
+        pixels[0]=(encryptPixel(pixels[0])).get_ui() % 100000;
+        pixels[1]=(encryptPixel(pixels[1])).get_ui() % 100000;
+        pixels[2]=(encryptPixel(pixels[2])).get_ui() % 100000;
+
+        pixels += image.channels();
   }
     view.sync();
     image.display();
@@ -73,7 +81,7 @@ try {
 void PixelEncryptor::decryptPixels()
 {
   Image image;
-  
+
 try {
     // Read a file into image object
     image.read("EncryptedPixelbypixel.png" );
@@ -82,13 +90,15 @@ try {
     //forwardFourierTransformImage(&FFTresult,image);
     //Make magnitude visible by using logarythmic scale
     Pixels view(image);
-    PixelPacket *pixels = view.get(0,0,image.columns(),image.rows());
+    Quantum *pixels = view.get(0,0,image.columns(),image.rows());
+    int index{0};
     for(size_t i=0; i<image.columns()*image.rows();i++)
   {
-        pixels->red=decryptPixel(pixels->red);
-        pixels->green=decryptPixel(pixels->green);
-        pixels->blue=decryptPixel(pixels->blue);
-        pixels++;
+        pixels[0]=(decryptPixel(encryptions[index++])).get_ui();
+        pixels[1]=(decryptPixel(encryptions[index++])).get_ui();
+        pixels[2]=(decryptPixel(encryptions[index++])).get_ui();
+
+        pixels += image.channels();
   }
     view.sync();
     image.display();
@@ -99,4 +109,3 @@ try {
       cout << "Caught exception: " << error_.what() << endl;
     }
 }
-
--- newPNGEncryption-master/PixEncryptor.hpp
+++ newPNGEncryption-master/PixEncryptor.hpp
@@ -2,12 +2,18 @@
 #include <iostream>
 #include <vector>
 
+#include <gmpxx.h>
+
 class PixelEncryptor
 {
   private:
   std::string fileName;
+  std::vector<mpz_class> encryptions;
 
   public:
+  mpz_class encryptPixel(mpz_class pixelinput);
+  mpz_class decryptPixel(mpz_class pixelinput);
+
   PixelEncryptor(std::string fileName_);
   void encryptPixels();
   void decryptPixels();
